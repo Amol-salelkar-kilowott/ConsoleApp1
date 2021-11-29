7bb1143254cf8eb4c206c53a7c1851af1288c6b4
Amol-salelkar-kilowott <amol.salelkar@kilowott.com>
2021-11-29 15:03:30 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Amol", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeWithdrawal(3500, DateTime.Now, "Shoes");
+            Console.WriteLine(account.Balance);
+
+            account.MakeDeposit(5000, DateTime.Now, "Gpay");
+            Console.WriteLine(account.Balance);
+
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
